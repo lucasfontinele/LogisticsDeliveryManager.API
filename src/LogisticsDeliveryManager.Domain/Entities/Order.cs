5b41a6e8da8270a5440aa9eef05d7af23747bfc7
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-03-26 11:18:21 -0300

    feat: add basic order entity properties
    Introduce Id, Customer, Status, and IsPriority fields to the Order entity to establish its core structure.

--- src/LogisticsDeliveryManager.Domain/Entities/Order.cs
+++ src/LogisticsDeliveryManager.Domain/Entities/Order.cs
@@ -1,6 +1,11 @@
 namespace LogisticsDeliveryManager.Domain.Entities;
 
+using LogisticsDeliveryManager.Domain.Enums;
+
 public class Order
 {
-    
+    public long Id { get; set; }
+    public Customer Customer { get; set; }
+    public OrderStatus Status { get; set; }
+    public bool IsPriority { get; set; }
 }
