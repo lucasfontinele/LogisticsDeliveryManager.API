bf41d145ca1fc6363991ad3615976df95c61c34c
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-03-26 11:36:48 -0300

    refactor(domain): change ShippingStatuses.ShippingId to navigation property

--- src/LogisticsDeliveryManager.Domain/Entities/ShippingStatuses.cs
+++ src/LogisticsDeliveryManager.Domain/Entities/ShippingStatuses.cs
@@ -5,7 +5,7 @@ namespace LogisticsDeliveryManager.Domain.Entities;
 public class ShippingStatuses
 {
     public long Id { get; set; }
-    public long ShippingId { get; set; }
+    public Shipping Shipping { get; set; }
     public DateOnly EstimatedDeliveryDate { get; set; }
     public OrderStatus Status { get; set; }
     public DateTime CreatedAt { get; set; }
