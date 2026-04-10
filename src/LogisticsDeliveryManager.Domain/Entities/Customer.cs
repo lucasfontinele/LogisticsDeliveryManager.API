e7d7717f08c78e964ff4c8e1b1ae4b6ae3225645
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-04-10 15:38:44 -0300

    refactor(domain): encapsular endereços e melhorar a validação no Customer

--- src/LogisticsDeliveryManager.Domain/Entities/Customer.cs
+++ src/LogisticsDeliveryManager.Domain/Entities/Customer.cs
@@ -7,15 +7,60 @@ namespace LogisticsDeliveryManager.Domain.Entities;
 
 public class Customer : Person
 {
+    private readonly List<Address> _addresses = new();
+
     public CustomerType CustomerType { get; private set; }
-    public List<Address> Addresses { get; private set; }
+
+    public IReadOnlyCollection<Address> Addresses => _addresses.AsReadOnly();
 
     private Customer() { }
 
-    public Customer(string name, string document, string phoneNumber, Email email, CustomerType customerType, List<Address> addresses)
+    public Customer(
+        string name,
+        string document,
+        string phoneNumber,
+        Email email,
+        CustomerType customerType,
+        IEnumerable<Address> addresses)
         : base(name, document, phoneNumber, email)
     {
+        SetCustomerType(customerType);
+        AddAddresses(addresses);
+    }
+
+    private void SetCustomerType(CustomerType customerType)
+    {   
+
+        if (!Enum.IsDefined(typeof(CustomerType), customerType))
+            throw new ErrorOnValidationException(["Invalid customer type."]);
+            
         CustomerType = customerType;
-        Addresses = addresses;
+    }
+
+    public void AddAddress(Address address)
+    {
+        if (address is null)
+            throw new ErrorOnValidationException(["Address cannot be null"]);
+
+        _addresses.Add(address);
+    }
+
+    public void AddAddresses(IEnumerable<Address> addresses)
+    {
+        if (addresses is null)
+            throw new ErrorOnValidationException(["Addresses cannot be null"]);
+
+        foreach (var address in addresses)
+        {
+            AddAddress(address);
+        }
+    }
+
+    public void RemoveAddress(Address address)
+    {
+        if (address is null)
+            throw new ErrorOnValidationException(["Address cannot be null"]);
+
+        _addresses.Remove(address);
     }
 }
