5cff3d197437d67a3793c61ea531f27ffc391dca
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-04-10 18:44:24 -0300

    refactor(domain): adicionar validação de tipo de função na classe Employee

--- src/LogisticsDeliveryManager.Domain/Entities/Employee.cs
+++ src/LogisticsDeliveryManager.Domain/Entities/Employee.cs
@@ -1,6 +1,7 @@
 using LogisticsDeliveryManager.Domain.Enums;
 using LogisticsDeliveryManager.Domain.Entities.Base;
 using LogisticsDeliveryManager.Domain.ValueObjects;
+using LogisticsDeliveryManager.Exception.ExceptionsBase;
 
 namespace LogisticsDeliveryManager.Domain.Entities;
 
@@ -13,6 +14,19 @@ public class Employee : Person
     public Employee(string name, string document, string phoneNumber, Email email, RoleType roleType)
         : base(name, document, phoneNumber, email)
     {
+        Validate(roleType);
+        
         RoleType = roleType;
+    }
+
+    private static void Validate(RoleType roleType)
+    {
+        var errors = new List<string>();
+
+        if (!Enum.IsDefined(typeof(RoleType), roleType))
+            errors.Add("Invalid role type.");
+
+        if (errors.Any())
+            throw new ErrorOnValidationException(errors);
     }
 }
