283473efa9d3b7cf67c7fc42651540f9fa84f4b7
LCapistrano25 <leonardo.capistrano445@gmail.com>
2026-04-10 15:38:54 -0300

    refactor(domain): adicionar validação para o Driver e melhorar a gestão de erros

--- src/LogisticsDeliveryManager.Domain/Entities/Driver.cs
+++ src/LogisticsDeliveryManager.Domain/Entities/Driver.cs
@@ -1,4 +1,5 @@
 using LogisticsDeliveryManager.Domain.Enums;
+using LogisticsDeliveryManager.Exception.ExceptionsBase;
 
 namespace LogisticsDeliveryManager.Domain.Entities;
 
@@ -7,4 +8,24 @@ public class Driver
     public long Id { get; set; }
     public IEnumerable<DriverLicenseType> LicenseTypes { get; set; }
     public Employee Employee { get; set; }
+
+    public Driver(Employee employee, IEnumerable<DriverLicenseType> licenseTypes)
+    {
+        Employee = employee;
+        LicenseTypes = licenseTypes;
+    }
+
+    private static void Validate(Employee employee, IEnumerable<DriverLicenseType> licenseTypes)
+    {
+        var errors = new List<string>();
+
+        if (employee == null)
+            errors.Add("Employee cannot be null.");
+
+        if (licenseTypes == null || !licenseTypes.Any())
+            errors.Add("At least one driver license type must be provided.");
+
+        if (errors.Any())
+            throw new ErrorOnValidationException(errors);
+    }
 }
